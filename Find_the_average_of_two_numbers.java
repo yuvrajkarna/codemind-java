92056f6577525e4607e0f6e553e8170f25ed62cd
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-01-31 11:05:53 +0530

    import java.util.*; class ConversionTemp{     public static void main(String args[]){         int fr,cr;         float ans;         Scanner sc=new Scanner(System.in);         fr=sc.nextInt();         cr=sc.nextInt();         ans=fr+cr;         System.out.printf("%.4f",ans/2);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ConversionTemp{
+    public static void main(String args[]){
+        int fr,cr;
+        float ans;
+        Scanner sc=new Scanner(System.in);
+        fr=sc.nextInt();
+        cr=sc.nextInt();
+        ans=fr+cr;
+        System.out.printf("%.4f",ans/2);
+
+    }
+}
