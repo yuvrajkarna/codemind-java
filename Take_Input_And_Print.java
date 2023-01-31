ffa63f9d131f2e4675d58d5ff40b7f6e0c4e0ba2
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-01-31 11:41:07 +0530

    import java.util.*; class saple{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String str;         str=sc.nextLine();         System.out.println(str);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class saple{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str;
+        str=sc.nextLine();
+        System.out.println(str);
+    }
+}
