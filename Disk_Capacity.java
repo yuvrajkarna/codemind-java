ef624f30258aec39c1dd2e9200e4c08d7a7fe80c
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-01-31 11:54:11 +0530

    import java.util.*; class h{     public static void main(String args[]){         int a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.println(2*a*b*c*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class h{
+    public static void main(String args[]){
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(2*a*b*c*512);
+    }
+}
