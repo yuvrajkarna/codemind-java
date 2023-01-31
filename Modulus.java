23ca419e80e8baf71efaad5df5c5f6303a0a5b28
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-01-31 11:49:27 +0530

    import java.util.*; class h{     public static void main(String args[]){         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         System.out.println(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class h{
+    public static void main(String args[]){
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a%b);
+    }
+}
