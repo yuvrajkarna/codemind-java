d86b55e644a2ceea33fed70903195da966a2a78c
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-01-31 10:06:13 +0530

    import java.util.*; class sample{     public static void main(String args[]){         Scanner mySc=new Scanner(System.in);         float a=mySc.nextFloat();         System.out.printf("%.2f",a*a*3.14);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class sample{
+    public static void main(String args[]){
+        Scanner mySc=new Scanner(System.in);
+        float a=mySc.nextFloat();
+        System.out.printf("%.2f",a*a*3.14);
+    }
+}
