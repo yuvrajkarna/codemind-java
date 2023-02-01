444fbb805259c0548f8b9c7bab62eae4b214ad81
yuvrajkarna <97613180+yuvrajkarna@users.noreply.github.com>
2023-02-01 09:55:10 +0530

    import java.util.*; class ji{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=1;i<=n;i++){             for(int j=1;j<=n;j++){                 if( j==1 || j==n || j==i){                     System.out.printf("* ");                 }                 else System.out.printf("  ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,11 +4,11 @@ class ji{
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                if(i==1 || j==1 || i==n || j==i){
-                    System.out.printf("*");
+            for(int j=1;j<=n;j++){
+                if( j==1 || j==n || j==i){
+                    System.out.printf("* ");
                 }
-                else System.out.printf(" ");
+                else System.out.printf("  ");
             }
             System.out.println();
         }
